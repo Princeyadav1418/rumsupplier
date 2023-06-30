10bf17099d53182d684507aa0c0283d64b9a4e58
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-06-30 23:39:59 +0530

    B | R

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ lunch lineage_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
-export TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS=false
+# export TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS=false
 export SELINUX_IGNORE_NEVERALLOWS=true
 
 compile_plox () {
