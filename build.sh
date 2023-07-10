c55b506d5c60b980d7253d13570d74c878d34efb
Prashant-1695 <prashant33968@gmail.com>
2023-07-10 14:58:17 +0530

    B | V

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 # Normal build steps
 . build/envsetup.sh
-lunch lineage_lavender-userdebug
+lunch awaken_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 # export TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS=false
 export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_USERNAME=Prashant
+export BUILD_HOSTNAME=ArchLinux
 
 compile_plox () {
-mka bacon -j10
+make bacon -j10
 }
