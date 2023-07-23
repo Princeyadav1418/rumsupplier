0eb78321984add219a04d1e3689f8e7dcac6e000
Prashant-1695 <prashant33968@gmail.com>
2023-07-23 11:54:15 +0530

    B | V

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 # Normal build steps
 . build/envsetup.sh
-lunch awaken_lavender-userdebug
+lunch aosp_lavender-userdebug
+
+build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
 # export TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS=false
 export SELINUX_IGNORE_NEVERALLOWS=true
-export BUILD_USERNAME=Prashant
-export BUILD_HOSTNAME=ArchLinux
+
+exp_gapps () {
+export WITH_GAPPS=true
+}
 
 compile_plox () {
-make bacon -j10
+m otapackage -j10
 }
