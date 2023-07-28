993df0c367a1b64baea843ac7ae1fa8b7aaed519
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-07-28 16:56:43 +0530

    fix [skip ci]

--- build.sh
+++ build.sh
@@ -9,12 +9,9 @@ export TZ=Asia/Kolkata
 # export TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS=false
 export SELINUX_IGNORE_NEVERALLOWS=true
 export RELAX_USES_LIBRARY_CHECK=true
-export LEGION_BUILD_TYPE=K4.19
-
-# necessary for building gapps rom on retro dynamic
-export WITH_GMS=true
 
 exp_gapps () {
+export WITH_GMS=true
 export LEGION_GAPPS=true
 export WITH_GAPPS=true
 }
