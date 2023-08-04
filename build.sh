324ffc19c01b3436f38515f3589c93a38a1790bc
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-08-04 07:38:16 +0530

    B | R

--- build.sh
+++ build.sh
@@ -1,21 +1,28 @@
 # Normal build steps
 . build/envsetup.sh
-lunch legion_lavender-userdebug
+lunch evolution_lavender-userdebug
 
-build_gapps=1
+build_gapps=0
 
 # export variable here
 export TZ=Asia/Kolkata
-# export TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS=false
 export SELINUX_IGNORE_NEVERALLOWS=true
+#export BUILD_BROKEN_DUP_RULES=true
 export RELAX_USES_LIBRARY_CHECK=true
-
-exp_gapps () {
 export WITH_GMS=true
-export LEGION_GAPPS=true
+export EVO_BUILD_TYPE=K4.19
+export TARGET_USES_MINI_GAPPS=true
+
+exp_gapps() {
+export TARGET_GAPPS_ARCH=arm64
+export USE_GAPPS=true
+export ARROW_GAPPS=true
+export WITH_GMS=true
 export WITH_GAPPS=true
+export BLISS_BUILD_VARIANT=gapps
 }
 
 compile_plox () {
-make bacon -j10
+mka evolution -j17
+mv out/target/product/lavender/*lavender-ota*.zip out/target/product/lavender/evolution_lavender.zip
 }
