bf6475ed3d20563894eb21218d551da59be9ad46
Prashant-1695 <prashant33968@gmail.com>
2023-08-29 08:33:06 +0530

    B | V

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 # Normal build steps
 . build/envsetup.sh
-lunch aosp_lavender-userdebug
+lunch havoc_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 
 compile_plox () {
-mka bacon -j10
+make bacon -j10
 }
